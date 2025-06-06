6ca6769b9bda5e92323f2c681444085daa163e71
Official Bitcoin do Paraguai <jhonmaxell48@gmail.com>
2025-06-06 20:39:16 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+// Esta es tu página principal. Lo que pongas aquí se verá en tu dominio.
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>¡Hola, Mundo! Mi aplicación está funcionando.</h1>
+      <p>Este es el primer paso.</p>
+    </div>
+  );
+}
